69f77789b53714bc66dd2a25e80e96d492176b96
Enrique Solís Gálvez <enriquesolisgalvez@gmail.com>
2023-12-16 01:35:30 -0300

    ✨ Se agrega login y logout

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,35 @@
 import React from "react";
-import { Text, View } from "react-native";
-import PropTypes from "prop-types";
+import { Text, View, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
+import { firebase } from "../config";
 
 const Header = ({ title }) => {
+  const user = firebase.auth().currentUser;
+
+  async function logout() {
+    await firebase.auth().signOut();
+  }
+
   return (
-    <View>
+    <View
+      style={{
+        width: "100%",
+        flexDirection: "row",
+        justifyContent: "space-between",
+      }}
+    >
+      {user && <View style={{ width: 50 }} />}
       <Text style={{ fontWeight: "bold", fontSize: 20, color: "#fff" }}>
         {title}
       </Text>
+      {user && (
+        <TouchableOpacity onPress={() => logout()}>
+          <View style={{ width: 50 }}>
+            <AntDesign name="logout" size={24} color="white" />
+          </View>
+        </TouchableOpacity>
+      )}
     </View>
   );
 };
-
-Header.propTypes = {
-  title: PropTypes.string,
-};
-
-Header.defaultProps = {
-  title: "",
-};
-
 export default Header;
--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,76 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+  StyleSheet,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { firebase } from "../config";
 
 const Login = () => {
+  const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  async function loginUser() {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
   return (
-    <View>
-      <Text>Login</Text>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Email"
+        onChangeText={(text) => setEmail(text)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        value={email}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Contraseña"
+        onChangeText={(text) => setPassword(text)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        secureTextEntry
+        value={password}
+      />
+      <TouchableOpacity style={styles.button} onPress={() => loginUser()}>
+        <Text style={styles.buttonText}>Ingresar</Text>
+      </TouchableOpacity>
     </View>
   );
 };
 
-Login.defaultProps = {};
+const styles = StyleSheet.create({
+  container: { padding: 16, flex: 1, alignItems: "center" },
+  textInput: {
+    padding: 10,
+    width: "100%",
+    fontSize: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#000",
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  button: {
+    marginTop: 20,
+    width: "100%",
+    backgroundColor: "#3E4424",
+    padding: 16,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+  },
+});
 
 export default Login;
